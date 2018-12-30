9e2e4b7c30ccf686bb39fcb1ac24ac51efb8576d
FaustSJ <s_j_faust@comcast.net>
2018-12-30 17:59:21 -0500

    bwardiharhar

---
+++ scripts/question5hive.py
@@ -0,0 +1,33 @@
+import sys
+
+for line in sys.stdin:
+	splits = line.split('\t')
+        count = 0
+        country = 'none'
+        ind = 'none'
+        lastResult = 'None'
+        year = 'None'
+        firstYear = 0
+        secondYear = 0
+        firstVal = 0
+        secondVal = 0
+        for column in splits:
+        	if count == 0:
+                        country = str(column)
+                elif count == 2:
+                        ind = str(column)
+                elif count > 53:
+                        if str(column).strip() != '\N':
+                                if firstYear == 0:
+                                    firstYear = 1960 + count - 4
+                                    firstVal = float(column)
+                                else:
+                                    secondYear = 1960 + count - 4
+                                    secondVal = float(column)
+                count = count + 1
+        if (firstYear != secondYear) and (secondYear != 0) and (firstYear != 0) and (firstVal<1.0) and (secondVal>firstVal): 
+                difference = str(secondVal - firstVal)
+                firstYear = str(firstYear)
+                secondYear = str(secondYear)
+                result = '\t'.join([country, ind, firstYear, secondYear, difference])
+                print result
