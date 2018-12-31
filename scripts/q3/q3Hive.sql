4fe4c3573dedaa6478c9454a1cd43a6b726158d5
FaustSJ <s_j_faust@comcast.net>
2018-12-30 23:34:18 -0500

    Making Vince's changes

--- scripts/q3/q3Hive.sql
+++ scripts/q3/q3Hive.sql
@@ -1,13 +1,12 @@
 USE GENDER_STATISTICS;
 
+add file  /home/cloudera/q3HiveScript.py;
+
 DROP TABLE IF EXISTS QUESTION_3;
 
 CREATE TABLE QUESTION_3
 AS SELECT TRANSFORM (*)
-USING 'python hivescript.py' 
-AS (COUNTRY, INDICATION, CHANGE) 
+USING 'python testscript.py' 
+AS (COUNTRY, INDICATION, CHANGE)
 FROM GENDER_STATS
---where clauses change these
-WHERE INDICATOR_NAME LIKE 'Employment to population ratio, 15+, male (%) (modeled ILO estimate)%' 
-AND (INDICATOR_NAME NOT LIKE '%cumulative%') 
-AND (INDICATOR_NAME LIKE '% male%');
+WHERE INDICATOR_NAME='Employment to population ratio, 15+, male (%) (modeled ILO estimate)';
